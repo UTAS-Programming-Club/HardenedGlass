b682b3d99715556fa46a585a54d7fea5470fb6cc
Camtas <138750592+Camtas@users.noreply.github.com>
2026-03-01 20:49:13 +1100

    Basic Entity

--- src/client/kotlin/au/com/programmingclub/hardenedglass/HardenedGlassClient.kt
+++ src/client/kotlin/au/com/programmingclub/hardenedglass/HardenedGlassClient.kt
@@ -1,12 +1,93 @@
 package au.com.programmingclub.hardenedglass
 
+import au.com.programmingclub.hardenedglass.HardenedGlass.namespace
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry
+import net.minecraft.client.model.*
 import net.minecraft.client.render.RenderLayer
+import net.minecraft.client.render.VertexConsumer
+import net.minecraft.client.render.entity.EntityRendererFactory
+import net.minecraft.client.render.entity.MobEntityRenderer
+import net.minecraft.client.render.entity.model.EntityModel
+import net.minecraft.client.render.entity.model.EntityModelLayer
+import net.minecraft.client.render.entity.model.EntityModelPartNames
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.util.Identifier
+import net.minecraft.world.gen.densityfunction.DensityFunction
 
+
+@Environment(EnvType.CLIENT)
 object HardenedGlassClient : ClientModInitializer {
+  val MODEL_CUBE_LAYER: EntityModelLayer = EntityModelLayer(Identifier.of(HardenedGlass.namespace, "cube"), "main")
+
   override fun onInitializeClient() {
     // This entrypoint is suitable for setting up client-specific logic, such as rendering.
     BlockRenderLayerMap.INSTANCE.putBlock(HardenedGlass.HardenedGlassBlock, RenderLayer.getCutout())
+
+    EntityRendererRegistry.register(HardenedGlass.TestEntity, { context -> TestEntityRenderer(context) })
+
+    // In 1.17, use EntityRendererRegistry.register (seen below) instead of EntityRendererRegistry.INSTANCE.register (seen above)
+    EntityRendererRegistry.register<BaseTestEntity?>(
+      HardenedGlass.TestEntity,
+      EntityRendererFactory { context: EntityRendererFactory.Context? -> TestEntityRenderer(context!!) })
+
+    EntityModelLayerRegistry.registerModelLayer(MODEL_CUBE_LAYER, TestEntityModel::getTexturedModelData)
+  }
+}
+
+class TestEntityModel(modelPart : ModelPart) : EntityModel<BaseTestEntity>() {
+
+  private val base: ModelPart =  modelPart.getChild(EntityModelPartNames.CUBE)
+
+  companion object {
+    fun getTexturedModelData(): TexturedModelData {
+      val modelData = ModelData()
+      val modelPartData = modelData.root
+      modelPartData.addChild(
+        EntityModelPartNames.CUBE,
+        ModelPartBuilder.create().uv(0, 0).cuboid(-6f, 12f, -6f, 12f, 12f, 12f),
+        ModelTransform.pivot(0f, 0f, 0f)
+      )
+      return TexturedModelData.of(modelData, 64, 64)
+    }
+  }
+
+  override fun setAngles(
+    entity: BaseTestEntity,
+    limbAngle: Float,
+    limbDistance: Float,
+    animationProgress: Float,
+    headYaw: Float,
+    headPitch: Float
+  ) {
+  }
+
+  override fun render(
+    matrices: MatrixStack,
+    vertices: VertexConsumer,
+    light: Int,
+    overlay: Int,
+    red: Float,
+    green: Float,
+    blue: Float,
+    alpha: Float
+  ) {
+    listOf(base).forEach { modelRenderer ->
+      modelRenderer.render(matrices, vertices, light, overlay, red, green, blue, alpha)
+    }
+  }
+}
+
+class TestEntityRenderer(context: EntityRendererFactory.Context) : MobEntityRenderer<BaseTestEntity, TestEntityModel>(
+  context,
+  TestEntityModel(context.getPart(HardenedGlassClient.MODEL_CUBE_LAYER)),
+  0.5f
+) {
+  public override fun getTexture(entity: BaseTestEntity?): Identifier? {
+    return Identifier.of(namespace, "textures/entity/test/cube.png")
   }
 }
--- src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
+++ src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
@@ -2,11 +2,17 @@ package au.com.programmingclub.hardenedglass
 
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricDefaultAttributeRegistry
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry
 import net.minecraft.block.AbstractBlock
 import net.minecraft.block.Block
 import net.minecraft.block.Blocks.GLASS
 import net.minecraft.block.TransparentBlock
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.SpawnGroup
+import net.minecraft.entity.attribute.DefaultAttributeContainer
+import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.entity.mob.MobEntity
 import net.minecraft.entity.mob.PathAwareEntity
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
@@ -18,8 +24,9 @@ import net.minecraft.world.World
 
 
 object HardenedGlass : ModInitializer {
-  private val namespace = "hardenedglass"
+  val namespace = "hardenedglass"
 
+  // --------------- Hardened Glass ---------------
   private val HardenedGlassIdentifier = Identifier(namespace, "hardened_glass")
   public val HardenedGlassBlock = TransparentBlock(
     AbstractBlock.Settings.copy(GLASS)
@@ -28,27 +35,60 @@ object HardenedGlass : ModInitializer {
   )
   private val HardenedGlassItem = BlockItem(HardenedGlassBlock, Item.Settings())
 
+  // --------------- Test Block ---------------
   private val TestBlockIdentifier = Identifier(namespace, "test_block")
   public val TestBlockBlock = Block(
-    AbstractBlock.Settings.create().burnable()
+    AbstractBlock.Settings.create()
   )
   private val TestBlockItem = BlockItem(TestBlockBlock, Item.Settings())
+
+  // --------------- Test Entity ---------------
+  val TestEntity: EntityType<BaseTestEntity?>? = Registry.register<EntityType<*>?, EntityType<BaseTestEntity?>?>(
+    Registries.ENTITY_TYPE,
+    Identifier.of(namespace, "cube"),
+    EntityType.Builder.create<BaseTestEntity?>(EntityType.EntityFactory { entityType: EntityType<BaseTestEntity?>?, world: World? ->
+      BaseTestEntity(
+        entityType!!,
+        world!!
+      )
+    }, SpawnGroup.CREATURE).dimensions(0.75f, 0.75f).build("cube")
+  )
+  private val TestEntityIdentifier = Identifier(namespace, "test_entity")
+
 
   override fun onInitialize() {
     // This code runs as soon as Minecraft is in a mod-load-ready state.
     // However, some things (like resources) may still be uninitialized.
     // Proceed with mild caution.
+
+    // --------------- Hardened Glass ---------------
     Registry.register(Registries.BLOCK, HardenedGlassIdentifier, HardenedGlassBlock)
     Registry.register(Registries.ITEM, HardenedGlassIdentifier, HardenedGlassItem)
 
+    // --------------- Test Block ---------------
     Registry.register(Registries.BLOCK, TestBlockIdentifier, TestBlockBlock)
     Registry.register(Registries.ITEM, TestBlockIdentifier, TestBlockItem)
+
+    FabricDefaultAttributeRegistry.register(TestEntity, BaseTestEntity.createMobAttribute())
+
+    FlammableBlockRegistry.getDefaultInstance().add(TestBlockBlock, 3, 2)
 
     ItemGroupEvents.modifyEntriesEvent(ItemGroups.COLORED_BLOCKS).register({content ->
       content.add(HardenedGlassItem)
       content.add(TestBlockItem)
     })
+  }
+}
 
-
+// --------------- Test Entity ---------------
+class BaseTestEntity(entityType: EntityType<out PathAwareEntity?>, world: World) : PathAwareEntity(entityType, world) {
+  companion object {
+    fun createMobAttribute(): DefaultAttributeContainer.Builder {
+      return MobEntity.createMobAttributes()
+        .add(EntityAttributes.GENERIC_MAX_HEALTH, 25.0)
+        .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 2.0)
+        .add(EntityAttributes.GENERIC_JUMP_STRENGTH, 2.0)
+        .add(EntityAttributes.GENERIC_SCALE, 2.0)
+    }
   }
 }
