87734fe017f12d51df222e366e83c27c003da0df
Camtas <138750592+Camtas@users.noreply.github.com>
2026-03-01 18:33:28 +1100

    Basic Block

--- src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
+++ src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
@@ -3,14 +3,19 @@ package au.com.programmingclub.hardenedglass
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.minecraft.block.AbstractBlock
+import net.minecraft.block.Block
 import net.minecraft.block.Blocks.GLASS
 import net.minecraft.block.TransparentBlock
+import net.minecraft.entity.EntityType
+import net.minecraft.entity.mob.PathAwareEntity
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroups
-import net.minecraft.util.Identifier
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
+import net.minecraft.world.World
+
 
 object HardenedGlass : ModInitializer {
   private val namespace = "hardenedglass"
@@ -23,6 +28,12 @@ object HardenedGlass : ModInitializer {
   )
   private val HardenedGlassItem = BlockItem(HardenedGlassBlock, Item.Settings())
 
+  private val TestBlockIdentifier = Identifier(namespace, "test_block")
+  public val TestBlockBlock = Block(
+    AbstractBlock.Settings.create().burnable()
+  )
+  private val TestBlockItem = BlockItem(TestBlockBlock, Item.Settings())
+
   override fun onInitialize() {
     // This code runs as soon as Minecraft is in a mod-load-ready state.
     // However, some things (like resources) may still be uninitialized.
@@ -30,8 +41,14 @@ object HardenedGlass : ModInitializer {
     Registry.register(Registries.BLOCK, HardenedGlassIdentifier, HardenedGlassBlock)
     Registry.register(Registries.ITEM, HardenedGlassIdentifier, HardenedGlassItem)
 
+    Registry.register(Registries.BLOCK, TestBlockIdentifier, TestBlockBlock)
+    Registry.register(Registries.ITEM, TestBlockIdentifier, TestBlockItem)
+
     ItemGroupEvents.modifyEntriesEvent(ItemGroups.COLORED_BLOCKS).register({content ->
       content.add(HardenedGlassItem)
+      content.add(TestBlockItem)
     })
+
+
   }
 }
