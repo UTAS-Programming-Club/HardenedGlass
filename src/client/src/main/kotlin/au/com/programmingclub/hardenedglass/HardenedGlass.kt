3d3c6e39705b21c0ef9ae81f93cb189d88b92cbd
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2026-06-24 13:12:48 +1000

    Support Indev 20100207-1703
    TransparentBlock now has a hardcoded ID,
    I am not sure if this is because of the
    deobfuscation or some optimisation.
    Either way I had to switch to the super
    class and add the same overrides so
    that the block behaves similarly.

--- src/client/src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
+++ src/client/src/main/kotlin/au/com/programmingclub/hardenedglass/HardenedGlass.kt
@@ -1,24 +1,38 @@
 package au.com.programmingclub.hardenedglass
 
 import net.minecraft.block.Block
-import net.minecraft.block.TransparentBlock
 import net.minecraft.block.material.Material
 import net.minecraft.item.ItemStack
+import net.minecraft.world.World
 import java.util.Random
 
-class HardenedGlassBlock : TransparentBlock {
+class HardenedGlassBlock : Block {
     constructor() : super(
         100,
         Helpers.addOverride("/terrain.png", "/assets/hardenedglass/textures/block/hardened_glass/hardened_glass.png"),
-        Material.GLASS,
-        false
+        Material.GLASS
     ) {
         setStrength(10f)
         setBlastResistance(9f)
-        setSounds(GLASS_SOUNDS)
+        setSounds(METAL_SOUNDS)
         // setKey("hardened_glass")
     }
 
+    // From TransparentBock
+    override fun isSolid(): Boolean {
+        return false
+    }
+
+    override fun shouldRenderFace(world: World, x: Int, y: Int, z: Int, face: Int): Boolean {
+        val block = world.getBlock(x, y, z)
+        if (block == this.id) {
+            return false
+        }
+
+        return super.shouldRenderFace(world, x, y, z, face)
+    }
+
+    // from GlassBlock
     override fun getBaseDropCount(random: Random): Int {
         return 0
     }
