684b3d3066d7c6e32bda15f82ab8d9bc677d7915
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2026-07-05 18:40:49 +1000

    Move from groovy to gradle kotlin for build config

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+	id("net.fabricmc.fabric-loom-remap")
+	id("maven-publish")
+	id("org.jetbrains.kotlin.jvm")
+}
+
+version = providers.gradleProperty("mod_version")
+group = providers.gradleProperty("maven_group")
+
+loom {
+	splitEnvironmentSourceSets()
+
+	mods {
+		register("hardenedglass") {
+			sourceSet(sourceSets.main.get())
+			sourceSet(sourceSets.getByName("client"))
+		}
+	}
+}
+
+dependencies {
+	// To change the versions see the gradle.properties file
+	minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+	mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
+	modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+
+	// Fabric API. This is technically optional, but you probably want it anyway.
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
+	modImplementation("net.fabricmc:fabric-language-kotlin:${property("fabric_kotlin_version")}")
+}
+
+tasks.processResources {
+	val version = version
+	inputs.property("version", version)
+
+	filesMatching("fabric.mod.json") {
+		expand("version" to version)
+	}
+}
+
+kotlin.jvmToolchain(21)
+
+java {
+	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+	// if it is present.
+	// If you remove this line, sources will not be generated.
+	withSourcesJar()
+}
+
+tasks.jar {
+	val projectName = project.name
+	inputs.property("projectName", projectName)
+
+	from("LICENSE") {
+		rename { "${it}_$projectName" }
+	}
+}
+
+// configure the maven publication
+publishing {
+	publications {
+		register<MavenPublication>("mavenJava") {
+			from(components["java"])
+		}
+	}
+}
